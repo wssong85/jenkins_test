abee6b5fcc91ca761d4e2d17f420f8ba78136579
songwonseock <tablier@gmail.com>
2022-03-25 04:38:51 +0900

    update

--- sh/discovery_start.sh
+++ sh/discovery_start.sh
@@ -1,15 +1,15 @@
 
 
 
-#nohup java -jar discovery-0.0.1.jar > /dev/null 2>&1 &
+nohup java -jar discovery-0.0.1.jar > /dev/null 2>&1 &
 
-pipeline  {
-  stage('test') {
-      node('jenkins_local_slave'){
-          sh 'JENKINS_NODE_COOKIE=dontKillMe nohup /var/jenkins_home/workspace/jenkins_test/discovery/build/libs/discovery_start.sh > /dev/null 2>&1 &'
-      }
-  }
-}
+#pipeline  {
+#  stage('test') {
+#      node('jenkins_local_slave'){
+#          sh 'JENKINS_NODE_COOKIE=dontKillMe nohup /var/jenkins_home/workspace/jenkins_test/discovery/build/libs/discovery_start.sh > /dev/null 2>&1 &'
+#      }
+#  }
+#}
 
 #PNAME="discovery-0.0.1.jar"
 #PID=`ps -ef | grep ${PNAME} | grep -v grep | grep -v sh | awk '{printf($2);}'`
