bcdcfec4bf40437b560bf7ae5dee19377d9770c4
songwonseock <tablier@gmail.com>
2022-03-25 04:19:06 +0900

    update

--- sh/discovery_start.sh
+++ sh/discovery_start.sh
@@ -8,7 +8,7 @@ chmod 755 gradlew
 echo ">> move build/libs"
 cd ./build/libs
 echo ">> execute discovery"
-nohup java -jar discovery-0.0.1.jar > /dev/null 2>&1 &
+nohup java -jar discovery-0.0.1.jar 2>&1 &
 echo ">> process end"
 
 #PNAME="discovery-0.0.1.jar"
