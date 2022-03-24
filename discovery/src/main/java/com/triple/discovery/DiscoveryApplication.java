69b51fac36155fab333c64ceefbe3ae01d35835f
songwonseock <tablier@gmail.com>
2022-03-25 04:39:23 +0900

    update

--- discovery/src/main/java/com/triple/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/triple/discovery/DiscoveryApplication.java
@@ -10,7 +10,6 @@ public class DiscoveryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
-        System.out.print(0);
     }
 
 }
