e488cb7d66ced15b7f5e7af78df53d2ff1ab1af0
songwonseock <tablier@gmail.com>
2022-03-25 06:14:21 +0900

    update

--- sh/discovery_start.sh
+++ sh/discovery_start.sh
@@ -1,30 +1,17 @@
 
-
-
-nohup java -jar discovery-0.0.1.jar > /dev/null 2>&1 &
-
-#pipeline  {
-#  stage('test') {
-#      node('jenkins_local_slave'){
-#          sh 'JENKINS_NODE_COOKIE=dontKillMe nohup /var/jenkins_home/workspace/jenkins_test/discovery/build/libs/discovery_start.sh > /dev/null 2>&1 &'
-#      }
-#  }
-#}
-
-#PNAME="discovery-0.0.1.jar"
-#PID=`ps -ef | grep ${PNAME} | grep -v grep | grep -v sh | awk '{printf($2);}'`
-#if [ ${PID} ]; then
-#  echo "${PNAME} is already running! : ${PID}"
-#  exit 0
-#else
-#  echo "${PNAME} is started..."
-#fi
-#RESULT=$(java -jar ${PNAME} )
-#echo ">> setting..."
-#if [ ${RESULT} -eq 0 ]; then
-#  echo "done!!"
-#  exit 0
-#else
-#  echo "error: ${RESULT}"
-#  exit ${RESULT}
-#fi
+# jenkins-pipeline
+node {
+    stage ('- clone') {
+        git 'https://github.com/wssong85/jenkins_test.git'
+    }
+    stage ('gradle wrap') {
+        sh 'cd discovery'
+        sh 'chmod 755 gradlew'
+    }
+    stage ('gradlew build') {
+        sh './gradlew clear build'
+    }
+    stage ('application start') {
+        sh 'JENKINS_NODE_COOKIE=dontKillMe nohup java -jar /var/jenkins_home/workspace/jenkins_test/discovery/build/libs/discovery-0.0.1.jar > /dev/null 2>&1 &'
+    }
+}
