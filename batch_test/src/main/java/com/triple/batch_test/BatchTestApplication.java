18fca66e4d43a366684c0c3df1de857db1e7b210
songwonseock <tablier@gmail.com>
2022-03-26 01:57:20 +0900

    batch_test add

---
+++ batch_test/src/main/java/com/triple/batch_test/BatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.triple.batch_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchTestApplication.class, args);
+    }
+
+}
---
+++ batch_test/src/test/java/com/triple/batch_test/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.batch_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
